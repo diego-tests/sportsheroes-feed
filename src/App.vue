124369f46868bce4e14bbdd4e4cfaba56bb77c96
Diego Guzmán <guzman.dag@gmail.com>
2020-05-24 12:07:46 +0200

    Add a pagination system Fixes #9

--- src/App.vue
+++ src/App.vue
@@ -6,32 +6,61 @@
             :date="day.date"
             :activities="day.activities"
         />
+        <div
+            ref="bottom"
+            class="loader-wrapper"
+        >
+            <LoadingAnimation v-if="isFetching" />
+        </div>
     </section>
 </template>
 
 <script>
-import { GET_RECENT_ACTIVITIES } from './store/_actionTypes'
+import { GET_RECENT_ACTIVITIES, FETCH_NEW_PAGE } from './store/_actionTypes'
 import { ACTIVITIES_PER_DAY } from './store/_getterTypes'
-import { mapActions, mapGetters } from 'vuex'
+import { mapActions, mapState, mapGetters } from 'vuex'
 
 import DayActivities from './components/DayActivities/DayActivities'
+import LoadingAnimation from './components/LoadingAnimation'
 
 export default {
   name: 'App',
   components: {
     DayActivities,
+    LoadingAnimation,
+  },
+  data() {
+    return {
+      observer: new IntersectionObserver(this.onPageEnd),
+    }
   },
   computed: {
     ...mapGetters({ days: ACTIVITIES_PER_DAY }),
+    ...mapState({ isFetching: state=> state.isFetching }),
   },
   created() {
     this.GET_RECENT_ACTIVITIES()
   },
+  mounted() {
+    this.observer.observe(this.$refs.bottom)
+  },
   methods: {
-    ...mapActions({ GET_RECENT_ACTIVITIES }),
+    ...mapActions({ 
+      GET_RECENT_ACTIVITIES, 
+      FETCH_NEW_PAGE, 
+    }),
+    onPageEnd() {
+      this.FETCH_NEW_PAGE()
+    },
   },
 }
 </script>
 <style lang="scss">
 @use './scss/global';
+</style>
+<style lang="scss" scoped>
+.loader-wrapper {
+    margin: 1rem auto;
+    max-width: 20rem;
+}
 </style>
--- src/store/_mutationTypes.js
+++ src/store/_mutationTypes.js
@@ -1,1 +1,3 @@
-export const SET_ACTIVITIES = 'Set current user'
+export const SET_ACTIVITIES = 'Set Activities'
+export const SET_IS_FETCHING = 'Set Fetching'
+export const SET_SKIP = 'Set Skip'
